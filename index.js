81d40669a24aa8b4750d1633814af07dce5c9093
tomaserben <terben@csas.cz>
2023-11-29 09:51:12 +0100

    .

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-let years;
-let amount;
-
 document.getElementById('calculate').addEventListener('click', function() {
-    var amount = parseInt(document.getElementById('investment-amount').value);
-    var years = parseInt(document.getElementById('years').value);
-    var yearlyAppreciation = getYearlyAppreciation(amount);
+    const amount = parseInt(document.getElementById('investment-amount').value);
+    const allocation = parseInt(document.getElementById('allocation').value);
+    const years = parseInt(document.getElementById('years').value);
+    const yearlyAppreciation = getYearlyAppreciation(allocation);
 
     console.log('Amount:', amount, 'Years:', years, 'Yearly Appreciation:', yearlyAppreciation);
 
-    var finalValue = calculateFinalValue(amount, yearlyAppreciation, years);
+    const finalValue = calculateFinalValue(amount, yearlyAppreciation, years);
     document.getElementById('result').textContent = 'Výsledná hodnota: ' + finalValue.toFixed(2) + ' USD';
 });
 
-function getYearlyAppreciation(amount) {
-    if (amount < 100) return Math.pow(1.01, 12);
-    if (amount < 250) return Math.pow(1.011, 12);
-    if (amount < 500) return Math.pow(1.012, 12);
-    if (amount < 1000) return Math.pow(1.013, 12);
-    if (amount < 2500) return Math.pow(1.014, 12);
-    if (amount < 5000) return Math.pow(1.015, 12);
-    if (amount < 10000) return Math.pow(1.016, 12);
-    if (amount < 25000) return Math.pow(1.017, 12);
-    if (amount < 50000) return Math.pow(1.018, 12);
-    if (amount < 100000) return Math.pow(1.019, 12);
-    if (amount >= 100000) return Math.pow(1.02, 12);
+const appreciation = {
+    0: 1.01,
+    100: 1.011,
+    250: 1.012,
+    500: 1.013,
+    1000: 1.014,
+    2500: 1.015,
+    5000: 1.016,
+    10000: 1.017,
+    25000: 1.018,
+    50000: 1.019,
+    100000: 1.02,
+};
+
+function getYearlyAppreciation(stakedAmount) {
+     return Math.pow(appreciation[stakedAmount], 12);
 }
 
 function calculateFinalValue(amount, yearlyAppreciation, years) {
@@ -34,15 +36,8 @@ function calculateFinalValue(amount, yearlyAppreciation, years) {
 // Sync range and number inputs
 document.getElementById('investment-amount').addEventListener('input', syncInputs);
 document.getElementById('amount-input').addEventListener('input', syncInputs);
-document.getElementById('years').addEventListener('input', syncInputs);
-document.getElementById('years-input').addEventListener('input', syncInputs);
 
 function syncInputs(event) {
-    console.log(event.target.value);
-    var targetId = event.target.id.includes('amount') ? 'investment-amount' : 'years';
-    var inputId = targetId === 'investment-amount' ? 'amount-input' : 'years-input';
-    document.getElementById(targetId).value = event.target.value;
-    // document.getElementById(targetId).step = event.target.value;
-    document.getElementById(inputId).value = event.target.value;
-    // document.getElementById(inputId).step = event.target.value;
+    document.getElementById('investment-amount').value = event.target.value;
+    document.getElementById('amount-input').value = event.target.value;
 }
